95b724676f9fd7c3f21e299f5d4d468f017849bb
SergioCayetano <sergiocayetano18@gmail.com>
2026-07-04 17:10:04 +0200

    Proyecto base con Vite + React

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="app">
+      <h1>SaldoCero</h1>
+      <p>Repartimos los gastos del viaje entre todos.</p>
+    </div>
+  );
+}
+
+export default App;
